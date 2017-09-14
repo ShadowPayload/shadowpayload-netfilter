e4e0e1c7ce68722d3056a6b3fb427edac1d3193a
Shadow <shadowpayload@gmail.com>
2017-09-14 12:17:44 -0400

    move to move_to_transport and new l4 check

--- src/payload_crypto.c
+++ src/payload_crypto.c
@@ -14,33 +14,40 @@
 
 static struct transport_info {
 	u8 protocol;
-	unsigned int offset;
+	unsigned char *transport_header;
 };
 
 static inline struct transport_info move_to_transport(struct iphdr *header4, struct ipv6hdr *header6) {
-	//TODO
-	while(ipv6_ext_hdr(header->nexthdr)) {
-
+	if (header4) {
+		if (header6) BUG();
+		return struct transport_info {
+			.protocol = header4->protocol,
+			.transport_header = (unsigned char *)header4 + header4->ihl * 4;
+		};
 	}
-	return struct transport_info {
-		.protocol = ;
-		.offset = ;
-	};
+
+	if (header6) {
+		//TODO
+		while(ipv6_ext_hdr(header->nexthdr)) {
+
+		}
+		return struct transport_info {
+			.protocol = ;
+			.transport_header = ;
+		};
+	}
+
+	BUG();
 }
 
-static inline bool check_l4_protocol(struct iphdr *l3_ip4_header, struct ipv6hdr *l3_ip6_header, crypto_option opt) {
-	/* The transport layer does not has to be raw in order to encrypt ip payload */
-	if (ENCRYPT == direction && IPPROTO_RAW == protocol)
-		return true;
-	if (l3_ip4_header != NULL)
-		return l3_ip4_header->protocol == protocol;
-	if (l3_ip6_header != NULL) {
-		return ipv6_move_to_transport(l3_ip6_header).protocol == protocol; //FIXME: this is wrong
-	}
+static inline bool check_l4_protocol(const struct transport_info *ti, crypto_option opt) {
+	if (DECRYPT_NETWORK == opt)
+		return IPPROTO_RAW == ti->protocol;
+	if (ENCRYPT_TRANSPORT == opt)
+		return IPPROTO_RAW != ti->protocol;
 }
 
 int transform_skb(sk_buff *skb, const struct crypto_info *ci, crypto_option opt) {
-	u8 l4_protocol;
 	/* beginning and end of encryption/decryption */
 	unsigned char *payload = NULL;
 	unsigned char *tail = skb->tail;
@@ -62,33 +69,34 @@ int transform_skb(sk_buff *skb, const struct crypto_info *ci, crypto_option opt)
 	else
 		l3_ip4_header = NULL;
 
-	/* validate layer 4 protocol */
-	check_l4_protocol(l3_ip4_header, l3_ip6_header, opt);
-
 	/* get layer 4 header and payload pointer */
 	struct transport_info ti = move_to_transport(l3_ip4_header, l3_ip6_header);
-	switch (protocol) {
+	if (!check_l4_protocol(&ti, opt)) {
+		printk(KERN_ERR "shadowpayload: invalid transport layer protocol");
+		return -EINVAL;
+	}
+	switch (ti.protocol) {
 	case IPPROTO_RAW:
-		payload = ;
+		payload = ti.transport_header;
 		break;
 	case IPPROTO_TCP:
-		l4_tcp_header = (struct tcphdr *)skb_transport_header(skb);
+		l4_tcp_header = (struct tcphdr *)ti.transport_header;
 		payload = (unsigned char *)l4_tcp_header + l4_tcp_header->doff * 4;
 		break;
 	case IPPROTO_UDP:
-		l4_udp_header = (struct l4_udp_header *)skb_transport_header(skb);
+		l4_udp_header = (struct l4_udp_header *)ti.transport_header;
 		payload = (unsigned char *)l4_udp_header + sizeof(struct udphdr);
 		break;
 	case IPPROTO_IPIP:
-		l4_ip4_header = (struct iphdr *)skb_transport_header(skb);
-		payload = (unsigned char *)l4_ip4_header + l4_ip4_header->ihl * 4;
+		l4_ip4_header = (struct iphdr *)ti.transport_header;
+		payload = move_to_transport(l4_ip4_header, NULL).transport_header;
 		break;
 	case IPPROTO_IPV6:
-		l4_ip6_header = (struct ipv6hdr *)skb_transport_header(skb);
-		payload = (unsigned char *)l4_ip6_header + ipv6_move_to_transport(l4_ip6_header).offset;
+		l4_ip6_header = (struct ipv6hdr *)ti.transport_header;
+		payload = move_to_transport(NULL, l4_ip6_header).transport_header;
 		break;
 	case IPPROTO_GRE:
-		l4_gre_header = (struct gre_full_hdr *)skb_transport_header(skb);
+		l4_gre_header = (struct gre_full_hdr *)ti.transport_header;
 		payload = NULL; //TODO
 		break;
 	default:
