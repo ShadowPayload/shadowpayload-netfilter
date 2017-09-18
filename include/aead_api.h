bafffc143cb105139a6e5ed8654a460a206832b0
Shadow <shadowpayload@gmail.com>
2017-09-17 20:06:23 -0400

    fix prototype

--- include/aead_api.h
+++ include/aead_api.h
@@ -5,13 +5,11 @@
 
 #define CCM_AAD_LEN	32
 
-struct crypto_aead *aead_key_setup_encrypt(const u8 key[],
-						    size_t key_len,
-						    size_t tag_len);
+struct crypto_aead *aead_key_setup_encrypt(const u8 key[], size_t key_len);
 int aead_encrypt(struct crypto_aead *tfm, u8 *b_0, u8 *aad, size_t aad_len,
-				 u8 *data, size_t data_len, u8 *tag, size_t tag_len);
+				 u8 *data, size_t data_len, u8 *auth);
 int aead_decrypt(struct crypto_aead *tfm, u8 *b_0, u8 *aad, size_t aad_len,
-				 u8 *data, size_t data_len, u8 *tag, size_t tag_len);
+				 u8 *data, size_t data_len, u8 *auth);
 void aead_key_free(struct crypto_aead *tfm);
 
 #endif /* _AEAD_API_H */
